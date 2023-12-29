4c7dcf2b0d76116c6db9d035c008280c09bad142
sextonb <brandon.sexton.1@outlook.com>
2023-12-29 15:43:50 -0700

    troubleshooting exports

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-export default {
-  convertAngle,
-  convertDistance,
-  convertTime,
-};
+export {convertDistance} from './src/convertDistance.js';
+export {convertAngle} from './src/convertAngle.js';
+export {convertTime} from './src/convertTime.js';
